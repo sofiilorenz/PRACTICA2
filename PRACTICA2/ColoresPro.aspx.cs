55146e162cf0162c71c4912cc10144dcbfed72cd
sofia lorenz <sofiiloreenz@gmail.com>
2026-07-11 16:12:52 -0300

    Add ColoresPro.aspx page with color selection feature
    Added ColoresPro.aspx Web Forms page featuring a DropDownList for color selection and a Label to display the chosen color. Implemented code-behind logic for populating options and handling selection changes. Included designer file and updated project file to register new assets.

---
+++ PRACTICA2/ColoresPro.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PRACTICA2
+{
+    public partial class ColoresPro : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ddl_colores_pro.Items.Add("Azul");
+                ddl_colores_pro.Items.Add("Rojo");
+                ddl_colores_pro.Items.Add("Verde");
+            }
+        }
+
+        //ENABLE AUTOPOSTBACK
+        protected void ddl_colores_pro_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lbl_color_pro.Text = ddl_colores_pro.SelectedItem.ToString();
+        }
+    }
+}
